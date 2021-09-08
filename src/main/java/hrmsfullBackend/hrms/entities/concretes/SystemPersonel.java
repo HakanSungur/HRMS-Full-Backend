ebbb7735a0cf9f468d6b162bd27e91fbbc669643
HakanSungur <hakansungur@gmail.com>
2021-09-09 00:57:54 +0300

    09.09.2021 00:57

--- src/main/java/hrmsfullBackend/hrms/entities/concretes/SystemPersonel.java
+++ src/main/java/hrmsfullBackend/hrms/entities/concretes/SystemPersonel.java
@@ -2,6 +2,7 @@ package hrmsfullBackend.hrms.entities.concretes;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
@@ -17,6 +18,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "system_personels", uniqueConstraints = {@UniqueConstraint(columnNames = {"username"})})
 @AllArgsConstructor
 @NoArgsConstructor
+@PrimaryKeyJoinColumn(name = "user_id", referencedColumnName = "id")
 public class SystemPersonel extends User {
 
 	@Column(name="username")
