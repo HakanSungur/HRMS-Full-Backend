fd0a85e82e44fb4f16f31d0ad34fa01981402877
HakanSungur <hakansungur@gmail.com>
2021-09-10 02:21:35 +0300

    10.09.2021 02:21

---
+++ src/main/java/hrmsfullBackend/hrms/entities/dtos/addDtos/CVAddDto.java
@@ -0,0 +1,18 @@
+package hrmsfullBackend.hrms.entities.dtos.addDtos;
+
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CVAddDto {
+
+	private int id;
+	private int jobSeekerId;
+	private String pictureUrl;
+	private String coverLetter="";
+}
