063ee2c61cffd9ad28b8a175cb4835fdd167a2b9
HakanSungur <hakansungur@gmail.com>
2021-09-16 12:03:53 +0300

    16.09.2021 12:03

--- src/main/java/hrmsfullBackend/hrms/core/adapters/EmailValidatorManagerAdapter.java
+++ src/main/java/hrmsfullBackend/hrms/core/adapters/EmailValidatorManagerAdapter.java
@@ -1,5 +1,27 @@
 package hrmsfullBackend.hrms.core.adapters;
 
-public class EmailValidatorManagerAdapter {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import hrmsfullBackend.hrms.core.services.EmailCheckService;
+import hrmsfullBackend.hrms.core.validators.EmailValidatorManager;
+import hrmsfullBackend.hrms.entities.concretes.User;
+
+@Service
+public class EmailValidatorManagerAdapter implements EmailCheckService {
+
+	private EmailValidatorManager emailValidatorManager;
+
+	@Autowired
+	public EmailValidatorManagerAdapter(EmailValidatorManager emailValidatorManager) {
+		super();
+		this.emailValidatorManager = emailValidatorManager;
+	}
+
+	@Override
+	public String emailValidator(User user) {
+		return this.emailValidatorManager.emailValidator(user);
+	}
+	
+	
 }
