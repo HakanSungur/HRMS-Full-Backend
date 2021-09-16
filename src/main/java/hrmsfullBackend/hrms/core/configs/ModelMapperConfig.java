2fafb5b5c70d820e640e67f9ba40c9fc83de39a3
HakanSungur <hakansungur@gmail.com>
2021-09-16 11:28:17 +0300

    16.09.2021 11:28

---
+++ src/main/java/hrmsfullBackend/hrms/core/configs/ModelMapperConfig.java
@@ -0,0 +1,17 @@
+package hrmsfullBackend.hrms.core.configs;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+	
+	@Bean
+	public ModelMapper getModelMapper() {
+		ModelMapper modelMapper=new ModelMapper();
+		modelMapper.getConfiguration().setAmbiguityIgnored(true);
+		return modelMapper;
+	}
+
+}
