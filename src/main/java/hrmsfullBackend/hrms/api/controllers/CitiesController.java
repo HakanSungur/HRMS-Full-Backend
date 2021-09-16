3943f50a8b042a51720688279ade0cf1eb392066
HakanSungur <hakansungur@gmail.com>
2021-09-17 00:05:07 +0300

    17.09.2021 00:04

---
+++ src/main/java/hrmsfullBackend/hrms/api/controllers/CitiesController.java
@@ -0,0 +1,33 @@
+package hrmsfullBackend.hrms.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import hrmsfullBackend.hrms.business.absracts.CityService;
+import hrmsfullBackend.hrms.core.utilities.results.DataResult;
+
+import hrmsfullBackend.hrms.entities.concretes.City;
+
+@RestController
+@RequestMapping("/api/cities")
+@CrossOrigin
+public class CitiesController {
+
+	private CityService cityService;
+
+	@Autowired
+	public CitiesController(CityService cityService) {
+		super();
+		this.cityService = cityService;
+	}
+	
+	@GetMapping("/getCities")
+	public DataResult<List<City>> getCities(){
+		return this.cityService.getCities();
+	}
+}
